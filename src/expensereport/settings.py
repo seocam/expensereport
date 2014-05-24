e6febd0846b9ec25cc48328a9dec94dc8df35a2d
Sergio Oliveira <sergio@tracy.com.br>
2014-05-24 16:40:51 -0300

    Fixing staticfiles_storage name

--- src/expensereport/settings.py
+++ src/expensereport/settings.py
@@ -91,7 +91,7 @@ MEDIA_ROOT = '/usr/share/nginx/expensereport/media/'
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATICFILES_STORAGE = 'storage.CachedStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CachedStaticFilesStorage'
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_URLS_REGEX = r'^/expense/api/.*$'
