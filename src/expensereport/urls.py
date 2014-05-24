c1c7be8385891325216ce1b93d3ef87734cbdd65
Sergio Oliveira <sergio@tracy.com.br>
2014-05-24 11:25:27 -0300

    Redirect home to dashboard

--- src/expensereport/urls.py
+++ src/expensereport/urls.py
@@ -2,14 +2,18 @@ from django.conf.urls import patterns, include, url
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', RedirectView.as_view(pattern_name='expenses:dashboard',
+                                    permanent=False)),
     url(r'^expense/', include('expenses.urls', namespace='expenses')),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', name="login"),
-    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', name="logout"),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout',
+        name="logout"),
 
 
     # Examples:
